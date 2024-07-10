c21df20a122f1827efc1b80af458c993ff0821ec
KaroBatis <93852645+karolyn1@users.noreply.github.com>
2024-07-09 21:11:46 -0600

    Finalizacion semana 8

---
+++ src/main/java/com/portafolio/domain/Producto.java
@@ -0,0 +1,47 @@
+
+package com.portafolio.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data //automatica me crea set and get
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private boolean activo;
+    private Long idCategoria;
+
+    public Producto() {
+    }
+
+    public Producto(String descripcion, String detalle, double precio, int existencias, String rutaImagen, boolean activo, Long idCategoria) {
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+        this.idCategoria = idCategoria;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/portafolio/service/ProductoService.java
@@ -0,0 +1,23 @@
+
+package com.portafolio.service;
+
+import com.portafolio.domain.Producto;
+import java.util.List;
+
+public interface ProductoService {
+    
+    //metodo que obtiene lista de categorias
+     // Se obtiene un listado de categorias en un List
+    public List<Producto> getProductos(boolean activos);
+    
+   // Se obtiene un prodiucto, a partir del id de un categoria
+    public Producto getProducto(Producto producto);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Producto producto);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Producto producto);
+    
+}
