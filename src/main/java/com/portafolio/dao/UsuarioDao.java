a8932d86a6acdfb4693f5814fa16d8f19b3e20ca
KaroBatis <93852645+karolyn1@users.noreply.github.com>
2024-07-30 20:56:12 -0600

    Finalizacion semana 11

--- src/main/java/com/portafolio/dao/UsuarioDao.java
+++ src/main/java/com/portafolio/dao/UsuarioDao.java
@@ -4,6 +4,13 @@ package com.portafolio.dao;
 import com.portafolio.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
-    Usuario findByUsername(String username); //busca por nombre de usuario
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+    Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password); //busca por password y nombre de usuario
+
+    Usuario findByUsernameOrCorreo(String username, String correo); //busca por correo, y nombre de usuario
+
+    boolean existsByUsernameOrCorreo(String username, String correo); //devuleve true o false
 }
+
